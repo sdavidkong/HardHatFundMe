9cc0f28d9d2fef859fd4a98f22bcdb7954ed561e
S David Kong <SDAVIDKONG@gmail.com>
2022-09-13 22:43:55 -0400

    configured mocks

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,9 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
   networks: {
     goerli: {
       url: RPC_URL_GOERLI,
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,6 +5,13 @@ const networkConfig = {
   },
 };
 
+const developmentChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_ANSWER = 2000;
+
 module.exports = {
   networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_ANSWER,
 };
